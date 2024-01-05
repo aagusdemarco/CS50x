0ecae4a7b38a7eaf37de01a40728703f97000836
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-05 04:32:22 +0000

    Fri, Jan 5, 2024, 1:32 AM -03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -12,6 +12,11 @@ int main(void)
 
     for(int i = 0; i < h; i++)
     {
-        
+        printf(" ");
+        for(int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
 }
