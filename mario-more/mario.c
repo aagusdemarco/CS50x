c4fb01179dc36e9fd266cf4df9dffcbe9c32e1ed
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-05 14:15:13 +0000

    Fri, Jan 5, 2024, 11:15 AM -03:00

---
+++ mario-more/mario.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    int h;
+
+    do
+    {
+        h = get_int("Height: ");
+    }
+    while (h < 1);
+
+    for (int i = 0; i < h; i++)
+    {
+        
+    }
+}
