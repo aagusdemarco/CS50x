9b67eb8307921a629eb05ba3aaa1d1c04dcb4927
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-07 15:42:32 +0000

    Sun, Jan 7, 2024, 12:42 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,16 +3,21 @@
 
 int main(void)
 {
-    long amex; //15 digitos y 34... o 37...
-    long visa; //13 o 16 digitos y 4...
-    long master; //16 digitos y 51..., 52..., 53..., 54..., 55...
+    long card;
+    int amex; //15 digitos y 34... o 37...
+    int visa; //13 o 16 digitos y 4...
+    int master; //16 digitos y 51..., 52..., 53..., 54..., 55...
     int counter = 0;
 
     do
     {
-        long card = get_long("Number: ");
+        card = get_long("Number: ");
     }
-    while(card < 0);
+    while (card < 0);
 
-    
+    while (card != 0)
+    {
+        card = card / 10;
+        counter++;
+    }
 }
