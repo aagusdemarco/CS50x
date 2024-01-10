a8083199dacc16aef78b40d92f99f7a81f6583e7
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-10 20:22:58 +0000

    Wed, Jan 10, 2024, 5:22 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,5 +6,8 @@
 int main(void)
 {
     int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-    string word = 
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    
 }
