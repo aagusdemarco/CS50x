c6925d6eb79aaf2ba9974a338ff649f0795755e2
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-10 20:42:59 +0000

    Wed, Jan 10, 2024, 5:42 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,6 +3,9 @@
 #include <ctype.h>
 #include <string.h>
 
+int convert_upper(string word);
+int get_score(string upperword);
+
 int main(void)
 {
     int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
@@ -12,15 +15,43 @@ int main(void)
     string upperword1 = convert_upper(word1);
     string upperword2 = convert_upper(word2);
 
+    int score1 = get_score(upperword1);
+    int score2 = get_score(upperword2);
 
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+        return 0
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+        return 0
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int convert_upper(string word)
 {
-    
+    int length = strlen(word);
+    for (int i = 0; i < length; i++)
+    {
+        word[i] = toupper(word[i]);
+        printf("%s\n", word);
+        return word;
+    }
 }
 
-int get_score(string word)
+int get_score(string upperword)
 {
-
+    int score = 0;
+    int length = strlen(upperword);
+    for (int i = 0; i < length; i++)
+    {
+        score = score + points[upperword[i] - 65];
+        return score;
+    }
 }
