bde0638260f241b57e09c0582e5a4a453cc84ba6
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-10 21:01:32 +0000

    Wed, Jan 10, 2024, 6:01 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,10 +6,10 @@
 string convert_upper(string word);
 int get_score(string upperword);
 void winner(int score1, int score2);
+int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void)
 {
-    int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
 
@@ -28,9 +28,8 @@ string convert_upper(string word)
     for (int i = 0; i < length; i++)
     {
         word[i] = toupper(word[i]);
-        printf("%s\n", word);
-        return word;
     }
+    return word;
 }
 
 int get_score(string upperword)
@@ -40,11 +39,11 @@ int get_score(string upperword)
     for (int i = 0; i < length; i++)
     {
         score = score + points[upperword[i] - 65];
-        return score;
     }
+    return score;
 }
 
-void winner(int score1; int score2)
+void winner(int score1, int score2)
 {
     if (score1 > score2)
     {
