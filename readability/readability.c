c0751c2b36eb165915fb8a5dfdc8d9bd1edc0f79
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-12 20:21:06 +0000

    Fri, Jan 12, 2024, 5:21 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,13 +7,46 @@ int main(void)
 {
     string text = get_string("Text: ");
     int length = strlen(text);
-    int index = 0;
-    int words = 0;
     int letters = 0;
+    int words = 0;
+    int sentences = 0;
 
     for (int i = 0; i < length; i++)
     {
+        // letter counter
+        char c = text[i];
+        if (isalpha(c) != 0)
+        {
+            letters++;
+        }
 
+        // word counter
+        if (c == ' ')
+        {
+            words++;
+        }
+
+        // sentence counter
+        if (c == '.' || c == '?' || c == '!')
+        {
+            sentences++;
+        }
+
+        // last word
+        words++;
+    }
+
+    int L = (letters / words) * 100;
+    int S = (sentences / words) * 100;
+    int index = 0.0588 * L - 0.296 * S - 15.8;
+
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
     }
 }
 
