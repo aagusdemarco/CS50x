5105e63bc2336c24a704240dd8d3a6ccba907b3d
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-12 20:25:18 +0000

    Fri, Jan 12, 2024, 5:25 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
@@ -55,4 +55,3 @@ int main(void)
         printf("Grade %i\n", index);
     }
 }
-
