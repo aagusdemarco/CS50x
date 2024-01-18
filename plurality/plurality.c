d65b81d64b216e505a09b41b58cf2bcb176c821f
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-18 21:12:32 +0000

    Thu, Jan 18, 2024, 6:12 PM -03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -65,14 +65,36 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    
-
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    int max_votes = 0;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > max_votes)
+        {
+            max_votes = candidates[i].votes;
+        }
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max_votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
     return;
 }
