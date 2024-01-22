96f361ce0384f4b233b64bc464e257c78be2d596
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-01-22 01:13:52 +0000

    Sun, Jan 21, 2024, 10:13 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(name, candidates[x].name) == 0)
         {
-            preferences
+            preferences[voter][rank] = x;
             return true;
         }
     }
