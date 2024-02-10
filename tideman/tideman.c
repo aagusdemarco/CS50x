95a884703b9b276b49e39803467f14dcf64b4bf1
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-02-10 23:34:40 +0000

    Sat, Feb 10, 2024, 8:34 PM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -153,7 +153,10 @@ void sort_pairs(void)
 {
     for (int i = 0; i < pair_count; i++)
     {
-        for (int j = i + 1; i < pair_count - 1;)
+        for (int j = i + 1; i < pair_count - 1; j++)
+        {
+            
+        }
     }
     return;
 }
