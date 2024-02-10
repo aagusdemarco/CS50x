310363dc303daa7f9d97eb909c3087f47f6e56ef
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-02-10 22:40:49 +0000

    Sat, Feb 10, 2024, 7:40 PM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -102,7 +103,7 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp(name, candidates[i]) == 0)
         {
-            ranks[rank] = i; 
+            ranks[rank] = i;
             return true;
         }
     }
