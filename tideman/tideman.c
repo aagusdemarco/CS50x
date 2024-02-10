e734ea8f584aa9e7cbee118e73d4c2cd09e2c573
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-02-10 23:26:21 +0000

    Sat, Feb 10, 2024, 8:26 PM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -151,7 +151,10 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    for (int i = 0; i < pair_count; )
+    for (int i = 0; i < pair_count; i++)
+    {
+        
+    }
     return;
 }
 
