97d7a995b322a53cfc22d4b503af274e212d3db8
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-02-10 22:39:16 +0000

    Sat, Feb 10, 2024, 7:39 PM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -102,7 +102,7 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp(name, candidates[i]) == 0)
         {
-            ranks[rank] = i;
+            ranks[rank] = i; 
             return true;
         }
     }
