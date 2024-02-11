75675549b6ba4edf930e110af9cb9cea892012c7
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-02-11 02:35:03 +0000

    Sat, Feb 10, 2024, 11:35 PM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -193,11 +193,11 @@ void lock_pairs(void)
 // Print the winner of the election
 void print_winner(void)
 {
-    for (int i = 0; i < pair_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        if (locked[pairs[i].winner][pairs[i].loser] == false)
+        for (int j = i + 1; j < candidate_count; i++)
         {
-            
+
         }
     }
     return;
