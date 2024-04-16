6f0fd260acf19e4014d11f5f5e636499e7ec3763
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-04-16 20:22:22 +0000

    Tue, Apr 16, 2024, 5:22 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,11 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop over all pixels (2-dimensional matrix)
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width ; j++)
         {
+            // Average of RGB values in all pixels
              int avg_rgb = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
+
         }
     }
     return;
