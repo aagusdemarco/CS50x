3509acaf7f455998f0591c196ad4407af97490b6
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-04-23 21:48:47 +0000

    Tue, Apr 23, 2024, 6:48 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -17,9 +17,17 @@ int main(int argc, char *argv[])
     // Create a buffer of 512 bytes for storing the jpegs
     uint8_t buffer[512];
 
+    int foundjpeg = 0;
+    int counter = 0;
+    char filename[8];
+    FILE *image;
+
     // Loop through the memory card
     while(fread(buffer, 1, 512, card))
     {
-        
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            if ()
+        }
     }
 }
