fa96209f625a2b2836651f40ff0135b2d299d0b2
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-01 23:51:04 +0000

    Wed, May 1, 2024, 8:51 PM -03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -39,6 +39,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
+    person *new_person = malloc(sizeof(person));
 
     // If there are still generations left to create
     if (generations > 1)
@@ -48,6 +49,7 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
     }
