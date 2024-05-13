d089caeb73b79afd5d837d1eea1dfdc967530a19
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-13 22:40:51 +0000

    Mon, May 13, 2024, 7:40 PM -03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,3 +2,27 @@ import cs50
 while True:
     c = cs50.get_float('Change owed: ')
 
+    if c >= 0:
+        break
+    else:
+        print('Invalid input. ')
+
+q = 25
+d = 10
+n = 5
+p = 1
+counter = 0
+
+while c > 0:
+    if c >= q:
+        c = c - q
+        counter += 1
+    elif c >= d and c < q:
+        c = c - d
+        counter += 1
+    elif c >= n and c < d:
+        c = c - p
+        counter += 1
+print(counter)
+
+
