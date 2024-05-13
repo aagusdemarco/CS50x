487c18a37537810b8c3a58828abc6c18cc6b8239
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-13 21:52:17 +0000

    Mon, May 13, 2024, 6:52 PM -03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,14 +3,13 @@ while True:
     h = cs50.get_int('Height: ')
 
     if h >= 1 and h <= 8:
-          break
+        break
     else:
-          print('Invalid input' )
+        print('Invalid input')
 
 for i in range(h):
     for j in range(h - i - 1):
-         print(' ', end='')
+        print(' ', end='')
     for k in range(i + 1):
-         print('#', end='')
+        print('#', end='')
     print()
-
