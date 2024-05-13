0761a4638afcd1ac5fdbbf9e028865e7acd656aa
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-13 21:27:48 +0000

    Mon, May 13, 2024, 6:27 PM -03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,13 @@
+while(True):
+    h = int(input('Height: '))
 
+    if h < 1:
+        print('Invalid Input. Please enter a positive integer. ')
+    else:
+        break
+for i in range(h):
+    for j in range(h - i):
+        print(' ')
+    for k in range(i - 1):
+        print('#')
+    print('\n')
