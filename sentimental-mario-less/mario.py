351df8af9700b5e60f8f7e998b8e0585ee6cafc5
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-13 21:33:53 +0000

    Mon, May 13, 2024, 6:33 PM -03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,13 +1,16 @@
 while(True):
     h = int(input('Height: '))
 
-    if h < 1 or type(h) != int or h = '':
+    if h < 1:
+        print('Invalid Input. Please enter a positive integer. ')
+    elif type(h) != int:
         print('Invalid Input. Please enter a positive integer. ')
     else:
+        for i < h:
+            for j in range(h - i):
+                print(' ')
+            for k in range(i + 1):
+                print('#')
+            print('\n')
         break
-for i < h:
-    for j in range(h - i):
-        print(' ')
-    for k in range(i + 1):
-        print('#')
-    print('\n')
+
