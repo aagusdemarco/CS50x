b3c63a6260475302dcd7513f6625d2cff4a98522
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-13 21:23:04 +0000

    Mon, May 13, 2024, 6:23 PM -03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,11 +1,1 @@
-h = int(input('Height: '))
 
-if h < 1:
-    for i in range(h):
-        for j in range(h-i):
-            print(' ')
-        for k in range(i + 1):
-            print('#')
-        print('\n')
-else:
-    
