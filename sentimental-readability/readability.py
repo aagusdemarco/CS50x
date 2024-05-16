13f580d2ccc7c710ba33fdf5c13d22e4d0b7eae8
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-16 23:04:22 +0000

    Thu, May 16, 2024, 8:04 PM -03:00

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,34 @@
+import cs50
+import txt
+
+text = cs50.get_string('Text: ')
+length = len(text)
+letters = 0
+words = 0
+sentences = 0
+
+for letter in text:
+
+    if txt.isalnum(letter) != 0:
+        letters += 1
+
+    if letter == ' ':
+        words += 1
+
+    if letter == '.' or c == '?' or c == '!':
+        sentences += 1
+
+words += 1
+
+L = (letters / float(words)) * 100
+S = (sentences / float(words)) * 100
+index = 0.0588 * L - 0.296 * S - 15.8
+index = round(index)
+
+if index >= 16:
+    print('Grade 16+')
+elif index < 1:
+    print('Before Grade 1')
+else:
+    print(f'Grade {index}')
+
