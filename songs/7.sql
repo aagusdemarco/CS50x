b471245077d2efcea8fa594a3486260375cd00fb
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-25 23:13:38 +0000

    Sat, May 25, 2024, 8:13 PM -03:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,1 +1,1 @@
-SELECT AVG(energy) FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = 'Drake')
+SELECT AVG(energy) FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = 'Drake');
