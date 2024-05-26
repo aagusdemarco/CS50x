eb7bf659c916126021f7e83b4648cb6b633c6e3f
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-26 20:32:42 +0000

    Sun, May 26, 2024, 5:32 PM -03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id WHERE person_id IN (SELECT id FROM people WHERE name = 'Bradley Cooper' OR name = 'Jennifer Lawrence');
+SELECT movies.title FROM movies JOIN stars ON movies.id = stars.movie_id WHERE person_id IN (SELECT id FROM people WHERE name = 'Bradley Cooper' OR name = 'Jennifer Lawrence');
