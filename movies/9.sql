900065fa66d30fe99dfd9ac5cbf16624f17ac972
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-26 00:39:52 +0000

    Sat, May 25, 2024, 9:39 PM -03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,1 +1,1 @@
-SELECT name, birth FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id = (SELECT id FROM movies WHERE year = 2004)) ORDER BY birth;
+SELECT name, birth FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004)) ORDER BY birth;
