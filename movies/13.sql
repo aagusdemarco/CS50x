e4ad43243fa4e5399844eff011dc72d2265ce7e6
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-26 20:22:40 +0000

    Sun, May 26, 2024, 5:22 PM -03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,2 @@
-SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id WHERE 
+SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id
+WHERE movies.id IN (SELECT movie_id FROM movies)
