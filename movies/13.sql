b2349c9087efac3a20cd397349779e91d2e222f4
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-26 20:26:47 +0000

    Sun, May 26, 2024, 5:26 PM -03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,2 +1,1 @@
-SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id
-WHERE movies.id IN (SELECT movie_id FROM movies)
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958))) AND people.name != 'Kevin Bacon';
