91d4ab412f600675b5ece407a22891bf6825f5fd
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-26 20:41:34 +0000

    Sun, May 26, 2024, 5:41 PM -03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,1 +1,1 @@
-SELECT name, birth FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004)) ORDER BY birth;
+SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id WHERE movies.title = 'Toy Story';
