a5c7d2f497f6ba3919a6c7303aefb968d63e86c6
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-26 00:15:42 +0000

    Sat, May 25, 2024, 9:15 PM -03:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,1 +1,1 @@
-SELECT AVG(rating) FROM ratings WHERE movie_id = (SELECT id FROM movies WHERE year = 2012);
+SELECT AVG(rating) FROM ratings JOIN movies on ratings.movie_id = movies.id WHERE year = 2012;
