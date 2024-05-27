bee3f29b586d6f0c556f4b2e845755590936f4eb
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-05-27 15:18:26 +0000

    Mon, May 27, 2024, 12:18 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Check the crime_scene_reports table for info on crimes comitted on Humphrey Street, July 28, 2023.
+
+SELECT * FROM crime_scene_reports
+WHERE day = 28 AND month = 7 AND year = 2023 AND street = 'Humphrey Street';
