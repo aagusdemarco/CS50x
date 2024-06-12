e8953fad36cb6e4aad461a5393c2e173d7323210
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2024-06-12 02:39:17 +0000

    Tue, Jun 11, 2024, 11:39 PM -03:00

--- finance/app.py
+++ finance/app.py
@@ -42,7 +42,33 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
+    if request.method == "POST":
+        symbol = request.form.get("symbol").upper()
+        shares = request.form.get("shares")
+        if not symbol:
+            return apology("must provide symbol")
+        elif not shares or no shares.isdigit() or int(shares) <= 0:
+            return apology("must provide a positive integer number of shares")
+
+        quote = lookup(symbol)
+        if quote is None:
+            return apology("symbol not found")
+
+        price = quote["price"]
+        total_cost = int(shares) * price
+        cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
+
+        if cash < total_cost:
+            return apology("not enough cash")
+
+        db.execute("UPDATE users SET cash = cash - :total_cost WHERE id = :user_id", total_cost=total_cost, user_id=session["user_id"])
+        db.execute("INSERT INTO transactions (user_id, symbol, shares,price) VALUES (:user_id, :symbol, :shares, :price)",
+                   user_id=session["user_id"], symbol=symbol, shares=shares, price=price)
+        flash(f"Bought {shares} shares of {symbol} for {usd(total_cost)}")
+        return redirect("/")
+    
+    else:
+        return render_template("buy.html")
 
 
 @app.route("/history")
